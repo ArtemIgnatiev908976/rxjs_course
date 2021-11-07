6a092cbb18c27a699ffdd3e1d0544f77ae21a636
IgnatevArtem <ignatev.artem.nik@yandex.ru>
2021-11-08 00:19:00 +0400

    Оптимизация стримов. отмена подписки

--- rxjs/src/app/app.component.ts
+++ rxjs/src/app/app.component.ts
@@ -1,16 +1,22 @@
 import { Component } from '@angular/core';
-import {interval} from 'rxjs'
+import {interval, Subscription} from 'rxjs'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  sub: Subscription
+
 constructor() {
  const intervalStream$ = interval(1000)
 
-  intervalStream$.subscribe((value) => {
+  this.sub = intervalStream$.subscribe((value) => {
     console.log(value)
   })
+}
+
+stop(){
+this.sub.unsubscribe()
 }
 }
